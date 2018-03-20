3954d9bb53528ca78d939829cd4358a74d1c4470
sukantking <suka.dx@gmail.com>
2018-03-20 12:43:15 +0530

    Update setup_vrm.sh

--- setup_vrm.sh
+++ setup_vrm.sh
@@ -1,13 +1,13 @@
 #"/veriumMiner/cpuminer -n 1048576 -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u sukantking.worker1 -p sukant"
 set -x \
-    && BUILD_DEPS="automake autoconf ca-certificates pkg-config libssl-dev libgmp-dev zlib1g-dev g++ git" \
+    && BUILD_DEPS="" \
     && apt-get update \
     && apt-get upgrade -y \
-    && apt-get --no-install-recommends install -y libcurl4-openssl-dev libjansson-dev $BUILD_DEPS \
+    && apt-get --no-install-recommends install -y make libcurl4-openssl-dev libjansson-dev automake autoconf ca-certificates pkg-config libssl-dev libgmp-dev zlib1g-dev g++ git \
     && git clone https://github.com/effectsToCause/veriumMiner \
     && cd veriumMiner \
     && ./build.sh \
-    && apt-get -y --auto-remove purge $BUILD_DEPS \
+    && apt-get -y --auto-remove purge automake autoconf ca-certificates pkg-config libssl-dev libgmp-dev zlib1g-dev g++ git \
     && rm -fr /var/lib/apt-/lists/* \
     && chmod +x ./../vrmcommand.sh
     && ./../vrmcommand.sh
